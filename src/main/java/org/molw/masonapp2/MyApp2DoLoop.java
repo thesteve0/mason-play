9aad4381df4265d2ab13fd0ea6f0666387898217
Steven Pousty <steve.pousty@gmail.com>
2023-05-11 12:24:01 -0700

    quick

--- src/main/java/org/molw/masonapp2/MyApp2DoLoop.java
+++ src/main/java/org/molw/masonapp2/MyApp2DoLoop.java
@@ -6,7 +6,11 @@ import sim.engine.SimState;
 public class MyApp2DoLoop {
 
     public static void main(String[] args) {
+
         String[]  myArgs = new String[]{"-for", "4"};
+
+        //This runs the simulation 4 times. Probably the preferable way to do it as long as I don't need some sort of custom
+        //way of scheduling
         SimState.doLoop(MySimState2.class, myArgs);
         System.exit(0);
     }
